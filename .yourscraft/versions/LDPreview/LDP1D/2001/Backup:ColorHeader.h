6dbdc0374acdabffa63b6d3687a4e5da22efb9b4
zenithwzj <61925066+zenithwzj@users.noreply.github.com>
2020-05-30 17:14:30 +0800

    Create Backup:ColorHeader.h

---
+++ .yourscraft/versions/LDPreview/LDP1D/2001/Backup:ColorHeader.h
@@ -0,0 +1,10 @@
+//COLORHEADER
+//AUTHOR:zenithwzj
+//INCLUDED:enum color,void hprintf(const char* Format,bool high),void ColorText(HWND hwnd,int bg,int fg)
+//There's an error in the Backup file!
+enum color{
+A,B
+}
+//ERROR POSITION:
+#$ERR$#
+zsqfunc hprintf(pv(2) as char,vas high) {ZSQfunc.GetFunc.pass()}
